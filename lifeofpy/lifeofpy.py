c6ce3b6a99ccf103f85b3bf22e2ce1f571140900
Andrea Staten <statenandrea33@gmail.com>
2023-03-30 11:02:39 -0400

    Added ipyleaflet to lifeofpy (miss draw control)

--- lifeofpy/lifeofpy.py
+++ lifeofpy/lifeofpy.py
@@ -2,6 +2,38 @@
 
 import string
 import random
+import ipyleaflet
+
+class Map(ipyleaflet.Map):
+
+    def __init__(self, center, zoom, **kwargs) -> None:
+
+        if "scroll_wheel_zoom" not in kwargs:
+            kwargs["scroll_wheel_zoom"] = True
+
+        super().__init__(center=center, zoom=zoom, **kwargs)
+
+    def add_search_control(self, position="topleft", **kwargs):
+        """Add a search control to the map.
+
+        Args:
+            **kwargs: Keyword arguments to pass to the search control.
+        """
+        if "url" not in kwargs:
+            kwargs["url"] = "https://nominatim.openstreetmap.org/search?format=json&q={s}"
+
+        search_control = ipyleaflet.SearchControl(position=position, **kwargs)
+        self.add_control(search_control)
+
+    def add_draw_control(self, **kwargs):
+        """Add a draw control to the map.
+
+        Args:
+            **kwargs: Keyword arguments to pass to the draw control.
+        """
+        draw_control = ipyleaflet.DrawControl(**kwargs)
+        self.add_control(draw_control)
+
 
 def generate_random_string(length=10, upper=False, digits=False, punctuation=False):
     """Generate a random string of a given length.
